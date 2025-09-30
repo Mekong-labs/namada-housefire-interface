19702fdb65c7bbd94a4fb32fe73acd20929ee38d
Jeremy | MekongLabs <jeremy@mekonglabs.tech>
2025-09-30 13:11:06 +0700

    Update build-webapp.sh

--- build-webapp.sh
+++ build-webapp.sh
@@ -26,15 +26,31 @@ fi
 apt-get update
 apt-get install build-essential
 
+apt-get install -y curl
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
+apt-get install -y clang
+apt-get install -y pkg-config
+apt-get install -y libssl-dev
+apt-get install -y protobuf-compiler
+apt-get install -y npm
+
+rustup target add wasm32-unknown-unknown
+rustup toolchain add nightly-2025-03-27
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+
 yarn || exit 1
 yarn prepare || exit 1
 
-yarn --cwd ${PKG_DIR} wasm:build:multicore || exit 1
-yarn --cwd ${PKG_DIR} build:only || exit 1
+yarn --cwd ${PKG_DIR} build || exit 1
 
 if [[ ! -d "$OUTPUT_DIR" ]]; then
   echo "Missing output directory: $OUTPUT_DIR" 1>&2
   exit 1
 fi
 
-mv "$OUTPUT_DIR" "$DEST_DIR"
+mkdir -p "$DEST_DIR"
+mv "$OUTPUT_DIR"/* "$DEST_DIR"
